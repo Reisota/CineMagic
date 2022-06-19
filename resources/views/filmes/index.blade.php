d34b54ebe0726c16eee026d62df043c02fae0c79
Rodrigo Reis <100767022+Reisota@users.noreply.github.com>
2022-06-19 19:42:44 +0100

    dia filmes_index

--- resources/views/filmes/index.blade.php
+++ resources/views/filmes/index.blade.php
@@ -7,8 +7,8 @@
 
             <div class="col-md-3 mb-3">
                 <label for="genero">Genero:</label>
-                <select id="genero" name ="genero" class="form-control">
-                <option value="">Selecionar genero</option>
+                <select id="genero" name="genero" class="form-control">
+                    <option value="">Selecionar genero</option>
                     @foreach ($generos as $code => $nome)
                     <option value="{{$code}}" {{$genero == $code ? 'selected' : ''}}>
                         {{$nome}}
@@ -18,7 +18,7 @@
 
             </div>
             <div class="col-md-6 mb-3">
-      
+
                 <label for="pesquisa">Pesquisar por:</label>
                 <input type="text" class="form-control" id="pesquisa" name="pesquisa" placeholder="Escreva algo" value="{{$pesquisa}}">
 
@@ -37,17 +37,17 @@
 
 
                 <div class="card-body">
-                
+
                     <h5 class="card-title"><a class="nav-link" href="{{ route('info',['id' => $filme->id]) }}">{{$filme->titulo}}</a></h5>
                     <p class="card-text">{{$filme->sumario}}</p>
                     <p class="card-text">Sessões: @foreach ($filme->sessoes as $sessao)
-                                                     @if($sessao->data >= date("Y-m-d"))
-                                                     dia:   {{date('d',strtotime($sessao->data))}} - 
-                                                        {{$sessao->horario_inicio}},
-                                                    @endif
-                                                    
-                                                    @endforeach
-                                                    </p>
+                        @if($sessao->data >= date("Y-m-d"))
+                        dia: {{date('d/m',strtotime($sessao->data))}} -
+                        {{$sessao->horario_inicio}},
+                        @endif
+
+                        @endforeach
+                    </p>
                 </div>
             </div>
         </div>
